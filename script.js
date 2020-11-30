f20bc64b9d278511dbfd32f1e784a532bf8dbdd4
XFalk <gdru94@gmail.com>
2020-11-30 21:02:52 +0300

    tasks 1 to 4 completed

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert('Hello world')
+const enterCount = document.querySelector('#enterCount');
+
+enterCount.addEventListener('click', () => {
+  addElement();
+});
+function addElement(){
+    const square = document.querySelector('#square');
+    const count = prompt("Введите количество пикселей на доске", 10);
+    square.innerHTML=``;
+    const countdiv = 50 /count;
+    const countdivmin = 720 /count;
+        for (let i = 0; i < count*count; i++){
+        const div = document.createElement('div');
+        square.appendChild(div);
+            div.classList.add('pixel');
+   div.style.cssText = `
+    width: ${countdiv}vw;
+    height: ${countdiv}vw;
+     min-width: ${countdivmin}px;
+    min-height: ${countdivmin}px;`;
+ };
+        const pixeles = document.querySelectorAll('.pixel');
+pixeles.forEach(pixel => pixel.addEventListener('click', () =>{
+   // console.log('yes');
+   pixel.style.background='black';
+    }));
+}
